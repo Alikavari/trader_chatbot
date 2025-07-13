7d6dee862d98b8a10e999c6a54e33453bfb2b74d
Mohsen <thelight.mn@gmail.com>
2025-07-13 14:25:40 +0330

    feat: add docker-build & update readme

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,4 @@
-## Creating Docker image
+#!/bin/bash
 
-```
 rye build --wheel --clean
-docker build . --tag your-image-name
-```
-
-E.g:
-```
 docker build . --tag ghcr.io/mohsennz/muon-chatbot-client:latest
-```
